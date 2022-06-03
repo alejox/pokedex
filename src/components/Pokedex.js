2ba48f65d614b8505b2dd41cf5c55e499eb7152f
alejox <alejoxdrums@gmail.com>
2022-06-02 21:51:32 -0500

    changed file name

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import PokemonCard from './PokemonCarD';
+import PokemonCard from './PokemonCard';
 
 
 const Pokedex = () => {
